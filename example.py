cc453bbbc4f6eb1233ee27e85c28c86487acefc5
Sherjil Ozair <sherjilozair@gmail.com>
2016-04-27 17:24:11 -0700

    printing

--- example.py
+++ example.py
@@ -4,20 +4,27 @@ from dqn import Agent
 
 num_episodes = 20
 
-env_name = sys.argv[1]
+env_name = sys.argv[1] if len(sys.argv) > 1 else "MsPacman-v0"
 env = gym.make(env_name)
 
-agent = Agent(state_size=env.observation_space.shape, 
-              number_of_actions=env.action_space.n, 
+agent = Agent(state_size=env.observation_space.shape,
+              number_of_actions=env.action_space.n,
               save_name=env_name)
 
 for e in xrange(num_episodes):
     observation = env.reset()
     done = False
     agent.new_episode()
+    total_cost = 0.0
+    total_reward = 0.0
+    frame = 0
     while not done:
-        env.render()
+        frame += 1
+        #env.render()
         action, values = agent.act(observation)
         #action = env.action_space.sample()
         observation, reward, done, info = env.step(action)
-        agent.observe(reward)
+        total_cost += agent.observe(reward)
+        total_reward += reward
+    print "total reward", total_reward
+    print "mean cost", total_cost/frame
